3a7f390d48e7c614afbf73407a8a19a6e790833a
dishita-simform <dishita.tank@simformsolutions.com>
2025-03-03 11:12:52 +0530

    Submitted Task 2

---
+++ Dishita_Python_Parenthesis.py
@@ -0,0 +1,24 @@
+def generate_parentheses(n):
+    result = []
+
+    def backtrack(current, open_count, close_count):
+        if len(current) == 2 * n:
+            result.append(current)
+            return
+        
+        if open_count < n:
+            backtrack(current + "(", open_count + 1, close_count)
+        
+        if close_count < open_count:
+            backtrack(current + ")", open_count, close_count + 1)
+
+    backtrack("", 0, 0)
+    return result
+
+# Taking user input
+n = int(input("Enter the Number of Pairs of Parentheses: "))
+
+if 1 <= n <= 8:
+    print(generate_parentheses(n))
+else:
+    print("Please Enter a Value Between 1 and 8.")
